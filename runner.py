8d6ed177b68727cbd1a7fdc49393f562d346f6a1
Julian Cui <jucui@redhat.com>
2021-04-02 12:23:01 -0400

    Add argparsing functionality and help messages

--- runner.py
+++ runner.py
@@ -1,38 +1,32 @@
+import argparse
 import subprocess
-import sys
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Outputs summary of command execution.')
+
+    parser.add_argument('command',
+                        help='the command to be run')
+
+    parser.add_argument('-c', type=int, default=1,
+                        help='number of times to run the given command')
+
+    parser.add_argument('--failed-count', type=int, default=0,
+                        help='number of allowed failed command invocation attempts before giving up')
+
+    parser.add_argument('--sys-trace', action='store_true',
+                        help='creates a log for: Disk IO, Memory, Processes/CPU Usage, Network Card Package Counters')
+
+    parser.add_argument('--call-trace', action='store_true',
+                        help='for each failed execution, add a log with all the system calls ran by the command')
+
+    parser.add_argument('--log-trace', action='store_true',
+                        help='for each failed execution, add the command output logs')
+
+    parser.add_argument('--debug', action='store_true',
+                        help='show each instruction executed by the script')
+
+    return parser
 
 if __name__ == "__main__":
-
-    count = 1
-    num_failed_attempts = 0
-    sys_trace_option = call_trace_option = log_trace_option = debug_option = help_option = False
-
-    # Retrieve options from command line args
-    i = 1
-    command_line = []
-    while i < len(sys.argv):
-        if sys.argv[i] == '-c':
-            count = int(sys.argv[i + 1])
-            i += 2
-            continue
-        elif sys.argv[i] == '--failed-count':
-            num_failed_attempts = int(sys.argv[i + 1])
-            i += 2
-            continue
-        elif sys.argv[i] == '--sys-trace':
-            sys_trace_option = True
-        elif sys.argv[i] == '--call-trace':
-            call_trace_option = True
-        elif sys.argv[i] == '--log-trace':
-            log_trace_option = True
-        elif sys.argv[i] == '--debug':
-            debug_option = True
-        elif sys.argv[i] == '--help':
-            help_option = True
-        else:
-            command_line.append(sys.argv[i])
-
-        i += 1
-
-    for i in range(count):
-        subprocess.run(command_line)
+    parser = create_parser()
+    args = parser.parse_args()
